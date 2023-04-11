e8d5ccd0e8f3a871846268391f0282e181135e34
Julien Bernard <julien.bernard@univ-fcomte.fr>
2023-04-11 23:32:10 +0200

    add xmake build script

---
+++ xmake.lua
@@ -0,0 +1,36 @@
+set_project("gf")
+set_version("0.0.1")
+
+add_requires("sdl2", "fmt")
+
+add_rules("mode.asan", "mode.tsan", "mode.ubsan", "mode.coverage", "mode.debug", "mode.releasedbg", "mode.release")
+
+set_warnings("all")
+set_languages("cxx17")
+
+target("gf2core0")
+    set_kind("shared")
+    add_defines("GF_CORE_BUILD")
+    add_files("library/core/*.cc")
+    add_includedirs("include", { public = true })
+    add_packages("fmt", { public = true })
+
+target("gf2graphics0")
+    set_kind("shared")
+    add_defines("GF_GRAPHICS_BUILD")
+    add_files("library/graphics/*.cc")
+    add_includedirs("include", { public = true })
+    add_packages("sdl2")
+    add_deps("gf2core0")
+
+target("gf2_core_tests")
+    set_kind("binary")
+    set_languages("cxx17")
+    add_files("tests/tests_core_*.cc", "tests/main.cc")
+    add_files("tests/googletest/googletest/src/gtest-all.cc")
+    add_includedirs("tests/googletest/googletest")
+    add_includedirs("tests/googletest/googletest/include")
+    add_deps("gf2core0")
+    if is_plat("linux") then
+        add_syslinks("pthread")
+    end
