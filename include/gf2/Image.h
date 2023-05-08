8cb1134378a32795b35b10582e37716f7a1b4061
Julien Bernard <julien.bernard@univ-fcomte.fr>
2023-05-08 12:56:13 +0200

    add Image

---
+++ include/gf2/Image.h
@@ -0,0 +1,52 @@
+// SPDX-License-Identifier: Zlib
+// Copyright (c) 2023 Julien Bernard
+#ifndef GF_IMAGE_H
+#define GF_IMAGE_H
+
+#include <cstdint>
+
+#include <filesystem>
+#include <vector>
+
+#include "Color.h"
+#include "Rect.h"
+#include "Vec2.h"
+
+namespace gf {
+  class InputStream;
+
+  enum PixelFormat {
+    Rgba32,
+    Rgb24,
+  };
+
+  class Image {
+  public:
+    Image();
+    Image(Vec2I size);
+    Image(Vec2I size, Color color);
+    Image(Vec2I size, const uint8_t* pixels, PixelFormat format = PixelFormat::Rgba32);
+    Image(const std::filesystem::path& filename);
+    Image(InputStream& stream);
+
+    Vec2I size() const;
+
+    Color operator()(Vec2I position) const;
+    void put_pixel(Vec2I position, Color color);
+
+    void save_to_file(const std::filesystem::path& filename) const;
+
+    Image sub_image(RectI area) const;
+
+    const uint8_t* raw_pixels() const;
+
+  private:
+    std::ptrdiff_t offset_from_position(Vec2I position) const;
+
+    Vec2I m_size;
+    std::vector<uint8_t> m_pixels;
+  };
+
+} // namespace gf
+
+#endif // GF_IMAGE_H
---
+++ library/core/Image.cc
@@ -0,0 +1,259 @@
+// SPDX-License-Identifier: Zlib
+// Copyright (c) 2023 Julien Bernard
+
+// clang-format off: main header
+#include <gf2/Image.h>
+// clang-format on
+
+#include <algorithm>
+
+// clang-format off: main header
+#define STB_IMAGE_STATIC
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
+
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#define STB_IMAGE_WRITE_STATIC
+#include <stb_image_write.h>
+// clang-format on
+
+#include <gf2/Log.h>
+#include <gf2/Stream.h>
+
+namespace gf {
+
+  namespace {
+    std::size_t compute_image_size(Vec2I size)
+    {
+      return 4 * static_cast<std::size_t>(size.w) * static_cast<std::size_t>(size.h);
+    }
+
+    uint8_t to_byte(float channel)
+    {
+      return static_cast<uint8_t>(channel * 255);
+    }
+
+    float to_float(uint8_t byte)
+    {
+      return static_cast<float>(byte) / 255.0f;
+    }
+
+    int callback_read(void* user, char* data, int size)
+    {
+      auto* stream = static_cast<InputStream*>(user);
+      return static_cast<int>(stream->read(Span<uint8_t>(reinterpret_cast<uint8_t*>(data), size))); // NOLINT
+    }
+
+    void callback_skip(void* user, int n)
+    {
+      auto* stream = static_cast<InputStream*>(user);
+      stream->skip(n);
+    }
+
+    int callback_eof(void* user)
+    {
+      auto* stream = static_cast<InputStream*>(user);
+      return static_cast<int>(stream->finished());
+    }
+
+  } // namespace
+
+  Image::Image()
+  : m_size(0, 0)
+  {
+  }
+
+  Image::Image(Vec2I size)
+  : m_size(size)
+  , m_pixels(compute_image_size(size), 0xFF)
+  {
+  }
+
+  Image::Image(Vec2I size, Color color)
+  : m_size(size)
+  , m_pixels(compute_image_size(size))
+  {
+    uint8_t* ptr = m_pixels.data();
+    const uint8_t r = to_byte(color.r);
+    const uint8_t g = to_byte(color.g);
+    const uint8_t b = to_byte(color.b);
+    const uint8_t a = to_byte(color.a);
+
+    for (int y = 0; y < m_size.h; ++y) {
+      for (int x = 0; x < m_size.w; ++x) {
+        ptr[0] = r;
+        ptr[1] = g;
+        ptr[2] = b;
+        ptr[3] = a;
+        ptr += 4;
+      }
+    }
+  }
+
+  Image::Image(Vec2I size, const uint8_t* pixels, PixelFormat format)
+  : m_size(size)
+  , m_pixels(compute_image_size(size))
+  {
+    assert(pixels != nullptr);
+
+    switch (format) {
+      case PixelFormat::Rgb24:
+        {
+          uint8_t* ptr = m_pixels.data();
+
+          for (int y = 0; y < m_size.h; ++y) {
+            for (int x = 0; x < m_size.w; ++x) {
+              ptr[0] = pixels[0];
+              ptr[1] = pixels[1];
+              ptr[2] = pixels[2];
+              ptr[3] = 0xFF; // set alpha to max (opaque)
+              ptr += 4;
+              pixels += 3;
+            }
+          }
+
+          break;
+        }
+
+      case PixelFormat::Rgba32:
+        std::copy_n(pixels, m_pixels.size(), m_pixels.data());
+        break;
+    }
+  }
+
+  Image::Image(const std::filesystem::path& filename)
+  : m_size(0, 0)
+  {
+    int n = 0;
+    uint8_t* pixels = stbi_load(filename.string().c_str(), &m_size.w, &m_size.h, &n, STBI_rgb_alpha);
+
+    if (m_size.w == 0 || m_size.h == 0 || pixels == nullptr) {
+      Log::error("Could not load image from file '{}': {}\n", filename.string(), stbi_failure_reason());
+      throw std::runtime_error("Could not load image from file");
+    }
+
+    m_pixels.resize(compute_image_size(m_size));
+    std::copy_n(pixels, m_pixels.size(), m_pixels.data());
+    stbi_image_free(pixels);
+  }
+
+  Image::Image(InputStream& stream)
+  : m_size(0, 0)
+  {
+    stbi_io_callbacks callbacks;
+    callbacks.read = &callback_read;
+    callbacks.skip = &callback_skip;
+    callbacks.eof = &callback_eof;
+
+    int n = 0;
+    uint8_t* pixels = stbi_load_from_callbacks(&callbacks, &stream, &m_size.w, &m_size.h, &n, STBI_rgb_alpha); // NOLINT
+
+    if (m_size.w == 0 || m_size.h == 0 || pixels == nullptr) {
+      Log::warning("Could not load image from stream: {}\n", stbi_failure_reason());
+      throw std::runtime_error("Could not load image from stream");
+    }
+
+    m_pixels.resize(compute_image_size(m_size));
+    std::copy_n(pixels, m_pixels.size(), m_pixels.data());
+    stbi_image_free(pixels);
+  }
+
+  Vec2I Image::size() const
+  {
+    return m_size;
+  }
+
+  Color Image::operator()(Vec2I position) const
+  {
+    if (position.x < 0 || position.x >= m_size.w || position.y < 0 || position.y >= m_size.h) {
+      return Transparent;
+    }
+
+    const uint8_t* ptr = m_pixels.data() + offset_from_position(position);
+    return { to_float(ptr[0]), to_float(ptr[1]), to_float(ptr[2]), to_float(ptr[3]) };
+  }
+
+  void Image::put_pixel(Vec2I position, Color color)
+  {
+    if (position.x < 0 || position.x >= m_size.w || position.y < 0 || position.y >= m_size.h) {
+      return;
+    }
+
+    uint8_t* ptr = m_pixels.data() + offset_from_position(position);
+    ptr[0] = to_byte(color.r);
+    ptr[1] = to_byte(color.g);
+    ptr[2] = to_byte(color.b);
+    ptr[3] = to_byte(color.a);
+  }
+
+  void Image::save_to_file(const std::filesystem::path& filename) const
+  {
+    if (m_size.w == 0 || m_size.h == 0 || m_pixels.empty()) {
+      return;
+    }
+
+    auto extension = filename.extension().string();
+
+    for (char& c : extension) {
+      c = std::tolower(c, std::locale::classic());
+    }
+
+    if (extension == ".png") {
+      stbi_write_png(filename.c_str(), m_size.w, m_size.h, 4, m_pixels.data(), 0);
+      return;
+    }
+
+    if (extension == ".bmp") {
+      stbi_write_bmp(filename.c_str(), m_size.w, m_size.h, 4, m_pixels.data());
+      return;
+    }
+
+    if (extension == ".tga") {
+      stbi_write_tga(filename.c_str(), m_size.w, m_size.h, 4, m_pixels.data());
+      return;
+    }
+
+    Log::error("Format not supported: '{}'\n", extension);
+  }
+
+  Image Image::sub_image(RectI area) const
+  {
+    const RectI current_area = RectI::from_size(m_size);
+    auto maybe_intersection = current_area.intersection(area);
+
+    if (!maybe_intersection) {
+      return {};
+    }
+
+    const Vec2I position = maybe_intersection->position();
+    const Vec2I size = maybe_intersection->size();
+
+    Image image(size);
+
+    const uint8_t* src = m_pixels.data() + offset_from_position(position);
+    uint8_t* dst = image.m_pixels.data();
+
+    for (int y = 0; y < size.h; ++y) {
+      std::copy_n(src, size.w, dst);
+      src += static_cast<std::ptrdiff_t>(4 * m_size.w);
+      dst += static_cast<std::ptrdiff_t>(4 * size.w);
+    }
+
+    return image;
+  }
+
+  const uint8_t* Image::raw_pixels() const
+  {
+    if (m_pixels.empty()) {
+      return nullptr;
+    }
+
+    return m_pixels.data();
+  }
+
+  std::ptrdiff_t Image::offset_from_position(Vec2I position) const
+  {
+    return static_cast<std::ptrdiff_t>(position.x + position.y * m_size.w) * 4;
+  }
+
+} // namespace gf
