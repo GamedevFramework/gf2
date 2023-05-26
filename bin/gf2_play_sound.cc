46bf11e21c39777480305f410c8f9e676f9b2bc5
Julien Bernard <julien.bernard@univ-fcomte.fr>
2023-05-26 11:06:27 +0200

    add some binaries to make tests

---
+++ bin/gf2_play_sound.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+
+#include <gf2/AudioInitializer.h>
+#include <gf2/AudioManager.h>
+#include <gf2/Sound.h>
+
+int main(int argc, char *argv[]) {
+  if (argc != 2) {
+    std::cerr << "gf2_play_sound <sound>" << std::endl;
+    return 1;
+  }
+
+  gf::AudioInitializer audio_initializer;
+  gf::AudioManager audio_manager;
+  gf::Sound sound(argv[1], { gf::ref(audio_manager) });
+  sound.start();
+
+  std::cout << "Press enter to stop...\n";
+  std::string s;
+  std::getline(std::cin, s);
+
+  sound.stop();
+  return 0;
+}
--- bin/gf2_tarball_listfiles.cc
+++ bin/gf2_tarball_listfiles.cc
@@ -5,6 +5,7 @@
 int main(int argc, char* argv[])
 {
   if (argc != 2) {
+    std::cerr << "gf2_tarball_listfiles <tarball>" << std::endl;
     return 0;
   }
 
