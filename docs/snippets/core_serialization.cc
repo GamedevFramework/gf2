fa5fdf651871385a5c2390a4a50adc0da6d01984
Julien Bernard <julien.bernard@univ-fcomte.fr>
2023-05-29 11:26:52 +0200

    finish serialization doc

---
+++ docs/snippets/core_serialization.cc
@@ -0,0 +1,38 @@
+#include <gf2/Serialization.h>
+
+#include <string>
+
+// tag::user_defined[]
+struct Foo {
+  double bar;
+  int32_t baz;
+  std::string qux;
+};
+
+template<typename Archive>
+Archive& operator|(Archive& ar, Foo& foo) {
+  return ar | foo.bar | foo.baz | foo.qux;
+}
+// end::user_defined[]
+
+// tag::version[]
+struct Entity {
+  uint64_t id;
+  uint64_t timestamp; // added in version 2
+};
+
+template<typename Archive>
+Archive& operator|(Archive& ar, Entity& entity) {
+  ar | entity.id;
+
+  if (ar.version() >= 2) {
+    ar | entity.timestamp;
+  }
+
+  return ar;
+}
+// end::version[]
+
+void doc_Serialisation() {
+
+}
