d42ee1726cc8c0aa648c657c685480663dcad0cb
Julien Bernard <julien.bernard@univ-fcomte.fr>
2023-06-05 02:54:25 +0200

    add Window

---
+++ include/gf2/Window.h
@@ -0,0 +1,85 @@
+// SPDX-License-Identifier: Zlib
+// Copyright (c) 2023 Julien Bernard
+#ifndef GF_WINDOW_H
+#define GF_WINDOW_H
+
+#include <string>
+
+#include "Clock.h"
+#include "Flags.h"
+#include "GraphicsApi.h"
+#include "Time.h"
+#include "Vec2.h"
+
+using SDL_Window = struct SDL_Window;
+
+namespace gf {
+
+  enum class WindowHints : uint32_t {
+    Resizable = 0x0001,
+    Visible = 0x0002,
+    Decorated = 0x0004,
+  };
+
+  template<>
+  struct EnableBitmaskOperators<WindowHints> : std::true_type {
+  };
+
+  class GF_GRAPHICS_API Window {
+  public:
+    Window(const std::string& title, Vec2I size, Flags<WindowHints> hints = All);
+    Window(const Window&) = delete;
+    Window(Window&& other) noexcept;
+    ~Window();
+
+    Window& operator=(const Window&) = delete;
+    Window& operator=(Window&& other) noexcept;
+
+    uint32_t id();
+
+    bool closed() const;
+    void close();
+
+    void set_title(const std::string& title);
+    std::string title();
+
+    void set_position(Vec2I position);
+    Vec2I position();
+
+    void set_size(Vec2I size);
+    Vec2I size();
+
+    void set_minimum_size(Vec2I size);
+    Vec2I minimum_size();
+
+    void set_maximum_size(Vec2I size);
+    Vec2I maximum_size();
+
+    void minimize();
+    bool minimized();
+
+    void maximize();
+    bool maximized();
+
+    void set_fullscreen(bool fullscreen = true);
+    void toggle_fullscreen();
+    bool fullscreen() const;
+
+    void set_resizable(bool resizable = true);
+    bool resizable();
+
+    void set_visible(bool visible = true);
+    bool visible();
+
+    void set_decorated(bool decorated = true);
+    bool decorated();
+
+  private:
+    SDL_Window* m_window = nullptr;
+    bool m_should_close = false;
+    bool m_fullscreen = false;
+  };
+
+}
+
+#endif // GF_WINDOW_H
---
+++ library/graphics/Window.cc
@@ -0,0 +1,264 @@
+// SPDX-License-Identifier: Zlib
+// Copyright (c) 2023 Julien Bernard
+
+// clang-format off: main header
+#include <gf2/Window.h>
+// clang-format on
+
+#include <stdexcept>
+#include <utility>
+
+#include <SDL2/SDL.h>
+#include <gf2/Log.h>
+
+namespace gf {
+
+  namespace {
+    Uint32 to_flags(Flags<WindowHints> hints)
+    {
+      Uint32 flags = SDL_WINDOW_ALLOW_HIGHDPI | SDL_WINDOW_VULKAN;
+
+      if (hints.test(WindowHints::Resizable)) {
+        flags |= SDL_WINDOW_RESIZABLE;
+      }
+
+      if (hints.test(WindowHints::Visible)) {
+        flags |= SDL_WINDOW_SHOWN;
+      } else {
+        flags |= SDL_WINDOW_HIDDEN;
+      }
+
+      if (!hints.test(WindowHints::Decorated)) {
+        flags |= SDL_WINDOW_BORDERLESS;
+      }
+
+      return flags;
+    }
+
+  }
+
+  Window::Window(const std::string& title, Vec2I size, Flags<WindowHints> hints)
+  {
+    auto flags = to_flags(hints);
+    m_window = SDL_CreateWindow(title.c_str(), SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, size.w, size.h, flags);
+
+    if (m_window == nullptr) {
+      Log::error("Failed to create a window: {}", SDL_GetError());
+      throw std::runtime_error("Failed to create a window.");
+    }
+  }
+
+  Window::Window(Window&& other) noexcept
+  : m_window(std::exchange(other.m_window, nullptr))
+  , m_should_close(other.m_should_close)
+  , m_fullscreen(other.m_fullscreen)
+  {
+  }
+
+  Window::~Window()
+  {
+    if (m_window != nullptr) {
+      SDL_DestroyWindow(m_window);
+    }
+  }
+
+  Window& Window::operator=(Window&& other) noexcept
+  {
+    std::swap(m_window, other.m_window);
+    std::swap(m_should_close, other.m_should_close);
+    std::swap(m_fullscreen, other.m_fullscreen);
+    return *this;
+  }
+
+  uint32_t Window::id()
+  {
+    assert(m_window);
+    const uint32_t id = SDL_GetWindowID(m_window);
+
+    if (id == 0) {
+      Log::warning("Failed to get the window id: {}", SDL_GetError());
+    }
+
+    return id;
+  }
+
+  bool Window::closed() const
+  {
+    return m_should_close;
+  }
+
+  void Window::close()
+  {
+    m_should_close = true;
+  }
+
+  void Window::set_title(const std::string& title)
+  {
+    assert(m_window);
+    SDL_SetWindowTitle(m_window, title.c_str());
+    // TODO: [SDL3] check return
+  }
+
+  std::string Window::title()
+  {
+    assert(m_window);
+    return SDL_GetWindowTitle(m_window);
+  }
+
+  void Window::set_position(Vec2I position)
+  {
+    assert(m_window);
+    SDL_SetWindowPosition(m_window, position.x, position.y);
+    // TODO: [SDL3] check return
+  }
+
+  Vec2I Window::position()
+  {
+    assert(m_window);
+    Vec2I position = {};
+    SDL_GetWindowPosition(m_window, &position.x, &position.y);
+    // TODO: [SDL3] check return
+    return position;
+  }
+
+  void Window::set_size(Vec2I size)
+  {
+    assert(m_window);
+    SDL_SetWindowSize(m_window, size.x, size.y);
+    // TODO: [SDL3] check return
+  }
+
+  Vec2I Window::size()
+  {
+    assert(m_window);
+    Vec2I size = {};
+    SDL_GetWindowSize(m_window, &size.x, &size.y);
+    // TODO: [SDL3] check return
+    return size;
+  }
+
+  void Window::set_minimum_size(Vec2I size)
+  {
+    assert(m_window);
+    SDL_SetWindowMinimumSize(m_window, size.x, size.y);
+    // TODO: [SDL3] check return
+  }
+
+  Vec2I Window::minimum_size()
+  {
+    assert(m_window);
+    Vec2I size = {};
+    SDL_GetWindowMinimumSize(m_window, &size.x, &size.y);
+    // TODO: [SDL3] check return
+    return size;
+  }
+
+  void Window::set_maximum_size(Vec2I size)
+  {
+    assert(m_window);
+    SDL_SetWindowMaximumSize(m_window, size.x, size.y);
+    // TODO: [SDL3] check return
+  }
+
+  Vec2I Window::maximum_size()
+  {
+    assert(m_window);
+    Vec2I size = {};
+    SDL_GetWindowMaximumSize(m_window, &size.x, &size.y);
+    // TODO: [SDL3] check return
+    return size;
+  }
+
+  void Window::minimize()
+  {
+    assert(m_window);
+    SDL_MinimizeWindow(m_window);
+  }
+
+  bool Window::minimized()
+  {
+    assert(m_window);
+    auto flags = SDL_GetWindowFlags(m_window);
+    return (flags & SDL_WINDOW_MINIMIZED) != 0;
+  }
+
+  void Window::maximize()
+  {
+    assert(m_window);
+    SDL_MaximizeWindow(m_window);
+  }
+
+  bool Window::maximized()
+  {
+    assert(m_window);
+    auto flags = SDL_GetWindowFlags(m_window);
+    return (flags & SDL_WINDOW_MAXIMIZED) != 0;
+  }
+
+  void Window::set_fullscreen(bool fullscreen)
+  {
+    assert(m_window);
+
+    if (fullscreen) {
+      SDL_SetWindowFullscreen(m_window, SDL_WINDOW_FULLSCREEN_DESKTOP);
+    } else {
+      SDL_SetWindowFullscreen(m_window, 0);
+    }
+
+    m_fullscreen = fullscreen;
+  }
+
+  void Window::toggle_fullscreen()
+  {
+    set_fullscreen(!m_fullscreen);
+  }
+
+  bool Window::fullscreen() const
+  {
+    return m_fullscreen;
+  }
+
+  void Window::set_resizable(bool resizable)
+  {
+    assert(m_window);
+    SDL_SetWindowResizable(m_window, resizable ? SDL_TRUE : SDL_FALSE);
+  }
+
+  bool Window::resizable()
+  {
+    assert(m_window);
+    auto flags = SDL_GetWindowFlags(m_window);
+    return (flags & SDL_WINDOW_RESIZABLE) != 0;
+  }
+
+  void Window::set_visible(bool visible)
+  {
+    assert(m_window);
+
+    if (visible) {
+      SDL_ShowWindow(m_window);
+    } else {
+      SDL_HideWindow(m_window);
+    }
+  }
+
+  bool Window::visible()
+  {
+    auto flags = SDL_GetWindowFlags(m_window);
+    return (flags & SDL_WINDOW_SHOWN) != 0;
+  }
+
+  void Window::set_decorated(bool decorated)
+  {
+    assert(m_window);
+    SDL_SetWindowBordered(m_window, decorated ? SDL_TRUE : SDL_FALSE);
+  }
+
+  bool Window::decorated()
+  {
+    assert(m_window);
+    auto flags = SDL_GetWindowFlags(m_window);
+    return (flags & SDL_WINDOW_BORDERLESS) == 0;
+  }
+
+}
