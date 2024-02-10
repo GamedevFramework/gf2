789ec17e6ac5f8623f4962f739786a513abd5093
Julien Bernard <julien.bernard@univ-fcomte.fr>
2024-02-10 20:29:05 +0100

    tweak clang-tidy

--- clang-tidy.sh
+++ clang-tidy.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-clang-tidy -header-filter=.* -p build include/gf2/**/*.h library/core/*.cc library/graphics/*.cc library/network/*.cc library/network/bits/* library/physics/*.cc library/imgui/*.cc examples/*.cc bin/*.cc  games/**/*.cc games/**/**/*.h games/**/**/*.cc
+clang-tidy -p build include/gf2/**/*.h library/core/*.cc library/graphics/*.cc library/network/*.cc library/network/bits/* library/physics/*.cc library/imgui/*.cc examples/*.cc bin/*.cc  games/**/*.cc games/**/**/*.h games/**/**/*.cc
