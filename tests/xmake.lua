d3938f46ae188b133417ea7788823bf8d3893c7b
Julien Bernard <julien.bernard@univ-fcomte.fr>
2024-02-26 20:21:23 +0100

    get gtest from a package

--- tests/xmake.lua
+++ tests/xmake.lua
@@ -7,13 +7,8 @@ if has_config("tests") then
         set_kind("binary")
         set_languages("cxx17")
         add_files("tests_core_*.cc", "main_core.cc")
-        add_files("googletest/googletest/src/gtest-all.cc", { warnings = "none" })
-        add_includedirs("googletest/googletest")
-        add_includedirs("googletest/googletest/include")
         add_deps("gf2core0")
-        if is_plat("linux") then
-            add_syslinks("pthread")
-        end
+        add_deps("gtest")
         set_configdir("$(buildir)/config/core_tests")
         set_configvar("GF_TEST_ASSETS_DIRECTORY", "$(projectdir)/tests/assets")
         add_configfiles("config.h.in", {pattern = "@(.-)@"})
@@ -23,25 +18,15 @@ if has_config("tests") then
         set_kind("binary")
         set_languages("cxx17")
         add_files("tests_network_*.cc", "main_network.cc")
-        add_files("googletest/googletest/src/gtest-all.cc", { warnings = "none" })
-        add_includedirs("googletest/googletest")
-        add_includedirs("googletest/googletest/include")
         add_deps("gf2network0")
-        if is_plat("linux") then
-            add_syslinks("pthread")
-        end
+        add_deps("gtest")
 
     target("gf2_audio_tests")
         set_kind("binary")
         set_languages("cxx17")
         add_files("tests_audio_*.cc", "main_audio.cc")
-        add_files("googletest/googletest/src/gtest-all.cc", { warnings = "none" })
-        add_includedirs("googletest/googletest")
-        add_includedirs("googletest/googletest/include")
         add_deps("gf2audio0")
-        if is_plat("linux") then
-            add_syslinks("pthread")
-        end
+        add_deps("gtest")
         set_configdir("$(buildir)/config/audio_tests")
         set_configvar("GF_TEST_ASSETS_DIRECTORY", "$(projectdir)/tests/assets")
         add_configfiles("config.h.in", {pattern = "@(.-)@"})
