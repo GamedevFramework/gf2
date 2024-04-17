e2b32596b8a64f3e8fe532c891dd638e4c84b175
Julien Bernard <julien.bernard@univ-fcomte.fr>
2024-04-17 12:05:10 +0200

    add ColorRamp

---
+++ include/gf2/core/ColorRamp.h
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: Zlib
+// Copyright (c) 2023 Julien Bernard
+#ifndef GF_COLOR_RAMP_H
+#define GF_COLOR_RAMP_H
+
+#include <map>
+
+#include "Color.h"
+#include "CoreApi.h"
+
+namespace gf {
+
+  class GF_CORE_API ColorRamp {
+  public:
+    bool empty() const
+    {
+      return m_mapping.empty();
+    }
+
+    void add_color_stop(float offset, const Color& color);
+    Color compute_color(float offset) const;
+
+  private:
+    float m_min = 0.0f;
+    float m_max = 0.0f;
+    std::map<float, Color> m_mapping;
+  };
+
+}
+
+#endif // GF_COLOR_RAMP_H
---
+++ library/core/ColorRamp.cc
@@ -0,0 +1,55 @@
+// SPDX-License-Identifier: Zlib
+// Copyright (c) 2023 Julien Bernard
+
+#include <gf2/core/ColorRamp.h>
+
+#include <cassert>
+
+namespace gf {
+
+  void ColorRamp::add_color_stop(float offset, const Color& color)
+  {
+    if (empty()) {
+      m_min = m_max = offset;
+    } else {
+      if (offset < m_min) {
+        m_min = offset;
+      }
+
+      if (offset > m_max) {
+        m_max = offset;
+      }
+    }
+
+    m_mapping.emplace(offset, color);
+  }
+
+  Color ColorRamp::compute_color(float offset) const
+  {
+    if (m_mapping.empty()) {
+      return White;
+    }
+
+    if (offset < m_min) {
+      return m_mapping.begin()->second;
+    }
+
+    if (offset > m_max) {
+      return m_mapping.rbegin()->second;
+    }
+
+    auto iterator = m_mapping.lower_bound(offset);
+    assert(iterator != m_mapping.end());
+    auto [t2, c2] = *iterator;
+
+    if (iterator == m_mapping.begin()) {
+      return c2;
+    }
+
+    --iterator;
+    auto [t1, c1] = *iterator;
+
+    return gf::lerp(c1, c2, (offset - t1) / (t2 - t1));
+  }
+
+}
