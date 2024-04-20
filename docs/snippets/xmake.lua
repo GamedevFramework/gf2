574dd43b9ccb495d58cacc52c4b20b58568ab19e
Julien Bernard <julien.bernard@univ-fcomte.fr>
2024-04-20 10:00:58 +0200

    conditionnaly build documentation examples

--- docs/snippets/xmake.lua
+++ docs/snippets/xmake.lua
@@ -1,4 +1,21 @@
-target("gf2coredoc")
-    set_kind("shared")
-    add_files("core_*.cc")
-    add_deps("gf2core0")
+option("doc-examples", { description = "Build documentation examples", default = false })
+
+if has_config("doc-examples") then
+    set_group("Documentation Examples")
+
+    target("gf2coredoc")
+        set_kind("shared")
+        add_files("core_*.cc")
+        add_deps("gf2core0")
+
+    target("gf2graphicsdoc")
+        set_kind("shared")
+        add_files("graphics_*.cc")
+        add_deps("gf2graphics0")
+
+    target("gf2networkdoc")
+        set_kind("shared")
+        add_files("network_*.cc")
+        add_deps("gf2network0")
+
+end
