aa8763aa6c1e82ecbc927509ceb76ff430ced67f
Julien Bernard <julien.bernard@univ-fcomte.fr>
2024-05-04 16:19:30 +0200

    try to fix static linking on Linux

--- docs/snippets/xmake.lua
+++ docs/snippets/xmake.lua
@@ -4,18 +4,21 @@ if has_config("doc-examples") then
     set_group("Documentation Examples")
 
     target("gf2coredoc")
-        set_kind("shared")
+        set_kind("binary")
         add_files("core_*.cc")
+        add_files("main.cc")
         add_deps("gf2core0")
 
     target("gf2graphicsdoc")
-        set_kind("shared")
+        set_kind("binary")
         add_files("graphics_*.cc")
+        add_files("main.cc")
         add_deps("gf2graphics0")
 
     target("gf2networkdoc")
-        set_kind("shared")
+        set_kind("binary")
         add_files("network_*.cc")
+        add_files("main.cc")
         add_deps("gf2network0")
 
 end
