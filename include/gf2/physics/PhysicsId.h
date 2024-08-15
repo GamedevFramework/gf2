240e20c43c7a8d18b56b0b0e1fbcb6e1c922e7c2
Julien Bernard <julien.bernard@univ-fcomte.fr>
2024-08-15 18:20:12 +0200

    forgot file

---
+++ include/gf2/physics/PhysicsId.h
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: Zlib
+// Copyright (c) 2023 Julien Bernard
+#ifndef GF_PHYSICS_ID_H
+#define GF_PHYSICS_ID_H
+
+#include <cstdint>
+
+namespace gf {
+
+  enum class PhysicsId : uintptr_t {};
+
+}
+
+#endif // GF_PHYSICS_ID_H
