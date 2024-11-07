a8f42024f7383e9303d1ec1300b873e0f28e3130
Julien Bernard <julien.bernard@univ-fcomte.fr>
2024-11-07 18:54:44 +0100

    add BundleBuilder

---
+++ include/gf2/framework/BundleBuilder.h
@@ -0,0 +1,61 @@
+// SPDX-License-Identifier: Zlib
+// Copyright (c) 2023 Julien Bernard
+#ifndef GF_BUNDLE_BUILDER_H
+#define GF_BUNDLE_BUILDER_H
+
+#include <filesystem>
+#include <vector>
+
+#include <gf2/core/ResourceBundle.h>
+
+#include "FrameworkApi.h"
+
+namespace gf {
+  class AudioManager;
+  class FontManager;
+  class RenderManager;
+  struct AnimationResource;
+  struct AnimationGroupResource;
+  struct AudioSourceResource;
+  struct NinePatchResource;
+  struct RichMapResource;
+  struct RichTextResource;
+  struct SpriteResource;
+  struct TextResource;
+
+  class GF_FRAMEWORK_API BundleBuilder {
+  public:
+    BundleBuilder(RenderManager* render_manager, FontManager* font_manager, AudioManager* audio_manager);
+
+    void add_in_bundle(const AnimationResource& resource);
+    void add_in_bundle(const AnimationGroupResource& resource);
+    void add_in_bundle(const AudioSourceResource& resource);
+    void add_in_bundle(const NinePatchResource& resource);
+    void add_in_bundle(const RichMapResource& resource);
+    void add_in_bundle(const RichTextResource& resource);
+    void add_in_bundle(const SpriteResource& resource);
+    void add_in_bundle(const TextResource& resource);
+
+    void add_raw_texture(const std::filesystem::path& path);
+    void add_raw_font(const std::filesystem::path& path);
+    void add_raw_map(const std::filesystem::path& path);
+    void add_raw_sound(const std::filesystem::path& path);
+    void add_raw_music(const std::filesystem::path& path);
+
+    ResourceBundle make_bundle() const;
+
+  private:
+    RenderManager* m_render_manager = nullptr;
+    FontManager* m_font_manager = nullptr;
+    AudioManager* m_audio_manager = nullptr;
+
+    std::vector<std::filesystem::path> m_textures;
+    std::vector<std::filesystem::path> m_fonts;
+    std::vector<std::filesystem::path> m_maps;
+    std::vector<std::filesystem::path> m_sounds;
+    std::vector<std::filesystem::path> m_musics;
+  };
+
+}
+
+#endif // GF_BUNDLE_BUILDER_H
---
+++ library/framework/BundleBuilder.cc
@@ -0,0 +1,134 @@
+// SPDX-License-Identifier: Zlib
+// Copyright (c) 2023 Julien Bernard
+
+#include <gf2/framework/BundleBuilder.h>
+
+#include <gf2/core/AnimationData.h>
+#include <gf2/core/AudioSourceData.h>
+#include <gf2/core/FontFace.h>
+#include <gf2/core/NinePatchData.h>
+#include <gf2/core/RichMapResource.h>
+#include <gf2/core/SpriteData.h>
+#include <gf2/core/TextData.h>
+
+#include <gf2/audio/Music.h>
+#include <gf2/audio/Sound.h>
+#include <gf2/graphics/Texture.h>
+#include <gf2/graphics/RichMap.h>
+
+namespace gf {
+  BundleBuilder::BundleBuilder(RenderManager* render_manager, FontManager* font_manager, AudioManager* audio_manager)
+  : m_render_manager(render_manager)
+  , m_font_manager(font_manager)
+  , m_audio_manager(audio_manager)
+  {
+  }
+
+  void BundleBuilder::add_in_bundle(const AnimationResource& resource)
+  {
+    for (const std::filesystem::path& texture : resource.textures) {
+      add_raw_texture(texture);
+    }
+  }
+
+  void BundleBuilder::add_in_bundle(const AnimationGroupResource& resource)
+  {
+    for (const std::filesystem::path& texture : resource.textures) {
+      add_raw_texture(texture);
+    }
+  }
+
+  void BundleBuilder::add_in_bundle(const AudioSourceResource& resource)
+  {
+    switch (resource.data.type) {
+      case AudioSourceType::Sound:
+        add_raw_sound(resource.filename);
+        break;
+      case AudioSourceType::Music:
+        add_raw_music(resource.filename);
+        break;
+    }
+  }
+
+  void BundleBuilder::add_in_bundle(const NinePatchResource& resource)
+  {
+    add_raw_texture(resource.texture);
+  }
+
+  void BundleBuilder::add_in_bundle(const RichMapResource& resource)
+  {
+    add_raw_map(resource.filename);
+  }
+
+  void BundleBuilder::add_in_bundle(const RichTextResource& resource)
+  {
+    add_raw_font(resource.default_font);
+    add_raw_font(resource.bold_font);
+    add_raw_font(resource.italic_font);
+    add_raw_font(resource.bold_italic_font);
+  }
+
+  void BundleBuilder::add_in_bundle(const SpriteResource& resource)
+  {
+    add_raw_texture(resource.texture);
+  }
+
+  void BundleBuilder::add_in_bundle(const TextResource& resource)
+  {
+    add_raw_font(resource.font);
+  }
+
+  void BundleBuilder::add_raw_texture(const std::filesystem::path& path)
+  {
+    m_textures.push_back(path);
+  }
+
+  void BundleBuilder::add_raw_font(const std::filesystem::path& path)
+  {
+    m_fonts.push_back(path);
+  }
+
+  void BundleBuilder::add_raw_map(const std::filesystem::path& path)
+  {
+    m_maps.push_back(path);
+  }
+
+  void BundleBuilder::add_raw_sound(const std::filesystem::path& path)
+  {
+    m_sounds.push_back(path);
+  }
+
+  void BundleBuilder::add_raw_music(const std::filesystem::path& path)
+  {
+    m_musics.push_back(path);
+  }
+
+  ResourceBundle BundleBuilder::make_bundle() const
+  {
+    // capture 'this' by copy in case the bundle builder is a local variable
+    ResourceBundle bundle([*this](ResourceBundle* bundle, ResourceManager* resource_manager, ResourceAction action) {
+      for (const std::filesystem::path& texture : m_textures) {
+        bundle->handle<Texture>(texture, m_render_manager, resource_manager, action);
+      }
+
+      for (const std::filesystem::path& font : m_fonts) {
+        bundle->handle<FontFace>(font, m_font_manager, resource_manager, action);
+      }
+
+      for (const std::filesystem::path& map : m_maps) {
+        bundle->handle<RichMap>(map, { m_render_manager, resource_manager }, resource_manager, action);
+      }
+
+      for (const std::filesystem::path& sound : m_sounds) {
+        bundle->handle<Sound>(sound, m_audio_manager, resource_manager, action);
+      }
+
+      for (const std::filesystem::path& music : m_musics) {
+        bundle->handle<Music>(music, m_audio_manager, resource_manager, action);
+      }
+    });
+
+    return bundle;
+  }
+
+}
